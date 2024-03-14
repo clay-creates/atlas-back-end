495fe3f2fefa436c08449ed5e861e76f3ad36463
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-14 22:52:32 +0000

    make 1-file

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -7,32 +7,28 @@ import sys
 
 def employee_todo(employee_id):
     """ Function works with employee todo's, shows total todo and completed"""
+    # Establish connection to API
     site_url = "https://jsonplaceholder.typicode.com/"
     employee_ext = f"{site_url}/users/{employee_id}"
     todo_ext = f"{site_url}/todos"
 
+    # Get request information from API
     employee_info = requests.get(employee_ext).json()
-    employee_name = employee_info['name']
-    todo_list = requests.get(todo_ext, params={"userId": employee_id}).json()
+    employee_name = employee_info.get('name')
+    todo_list = requests.get(f"{todo_ext}?userId={employee_id}").json()
 
+    # Prepare data for csv [[List of Lists]]
     csv_data = [['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']]
     for item in todo_list:
         csv_data.append([employee_id, employee_name, item["completed"], item["title"]])
 
+    # Write csv_data to .csv file
+    with open(f"{employee_id}.csv", mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(csv_data)
 
+    print(f"Data for employee {employee_id} has been exported to {employee_id}.csv")
 
-    # completed_todos = []
-    # for item in todo_list:
-    #     if item["completed"]:
-    #         completed_todos.append(item["title"])
-
-    # total_todo = len(todo_list)
-    # total_done = len(completed_todos)
-
-    # print ("Employee {0} is done with tasks({1}/{2}):"
-    #        .format(employee_name, total_done, total_todo))
-    # for todo in completed_todos:
-    #     print(f"\t {todo}")
 
 if __name__ == "__main__":
     employee_todo(int(sys.argv[1]))
